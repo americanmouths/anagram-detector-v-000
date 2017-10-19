610fcb3cffa4784386ccd2587d6f9c24a95e30bf
Kayla Hansen <shiverr@gmail.com>
2017-10-19 03:49:18 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+
+    possible_anagrams.detect {|i| i.split("").sort == @word}
+
+end
+
+
+end
